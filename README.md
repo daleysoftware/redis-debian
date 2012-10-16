1b9c0314d9c1c9595ad8c54c22a08a3298db74fb
Matt Pillar <mpillar@uwaterloo.ca>
2012-10-16 15:47:42 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ redis-debian
 ============
 
 Tools to build a lightweight debian package for customized redis builds.
+
+usage
+=====
+
+./build-redis-debian.sh <name> <version> <desc> <repo>
+
+where
+
+* redis-repository-directory: the directory where your redis repo is located.
+* name: the control name for the debian.
+* debian: the version of the debian.
+* description: appended to the standard redis description.
+
+Enjoy!
---
+++ build-redis-debian.sh
@@ -0,0 +1,90 @@
+#!/bin/bash -e
+
+#
+# Functions
+#
+
+usage()
+{
+    echo "Usage: $0 <name> <version> <desc> <repo>"
+}
+
+#
+# Check options
+#
+
+if [ $# -ne 4 ]
+then
+    usage
+    exit 1
+fi
+
+name=$1
+version=$2
+desc=$3
+repo=$4
+
+if [ ! -d "$repo" ]
+then
+    echo "Error: \"$repo\" does not exist."
+    usage
+    exit 1
+fi
+
+if [ ! -f "$repo/Makefile" ]
+then
+    echo "No Makefile in \"$repo\". Are you sure that's where redis is located?"
+    usage
+    exit 2
+fi
+
+if [ -z "$name" ]
+then
+    echo "Must supply non-zero length debian name."
+    usage
+    exit 3
+fi
+
+if [ -z "$version" ]
+then
+    echo "Must supply valid version number."
+    usage
+    exit 4
+fi
+
+#
+# Build redis and populate
+#
+
+echo "Building redis..."
+
+cwd=$(pwd)
+cd "$repo"
+make 1>/dev/null 2>/dev/null
+cd $cwd
+
+#
+# Create the actual debian
+#
+
+echo "Building debian..."
+
+mkdir -p src/usr/bin
+
+for e in benchmark check-aof check-dump cli server
+do
+    cp "$repo"/src/redis-$e src/usr/bin
+done
+
+echo "  $desc" >> src/DEBIAN/control
+echo "Package: $name" >> src/DEBIAN/control
+echo "Version: $version" >> src/DEBIAN/control
+
+dpkg-deb --build src $name-$version.deb
+
+#
+# Closing remarks and cleanup
+#
+
+git checkout -- src/DEBIAN/control
+echo "Your debian is ready! Enjoy!"
